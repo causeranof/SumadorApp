13ac9d81ea3549300adc6c7bd5f2dd94bb862988
Fernando Causerano <causeranof@gmail.com>
2018-04-23 14:30:13 -0300

    First commit

---
+++ app/src/main/java/com/example/fcauserano/sumadorapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.fcauserano.sumadorapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewNumber;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textViewNumber = findViewById(R.id.text_view_number);
+    }
+
+    public void clickSumar(View view) {
+        Integer valorNumero = Integer.parseInt(textViewNumber.getText().toString());
+        valorNumero += 1;
+        textViewNumber.setText(valorNumero.toString());
+    }
+
+    public void clickRestar(View view) {
+        Integer valorNumero = Integer.parseInt(textViewNumber.getText().toString());
+        if(valorNumero>0){
+            valorNumero -= 1;
+            textViewNumber.setText(valorNumero.toString());
+        }else {
+            Toast.makeText(this, "A donde vas? Ya estamos en 0!!!", Toast.LENGTH_LONG).show();
+        }
+    }
+}
